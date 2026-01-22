673942db575abf90d466042b1ae579b6bedf22b9
Vishnu Priyan S S <vishnu32510@gmail.com>
2026-01-21 20:18:58 -0500

    add pathurlstrategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'data/my_bloc_observer.dart';
 import 'portfolio_app.dart';
 
 void main() {
   Bloc.observer = MyBlocObserver();
+  usePathUrlStrategy();
   runApp(const PortfolioApp());
 }
